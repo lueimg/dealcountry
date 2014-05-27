b8bc0a151af8c9ef0f3298a54a4b0fca92b9bdf1
Luis Mori <luismori@glr.pe>
2014-05-27 15:00:22 -0500

    envio de emails parte 1

---
+++ sites/all/modules/custom/giftcard/egiftcard-beneficiario.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<table>
+		<tr class="cabecera">
+			<td>[LOGO]</td>
+		</tr>
+		<tr>
+			<table class="cuerpo">
+				<!-- <tr>
+					<td class="text-grande">[cliente_nombre] has recieved your $50.00 E-dealcountry.com gift card.</td>
+				</tr> -->
+				<!-- <tr>
+					<td>
+						<div class="text-normal oscuro" style="font-size: 16px; color:#555;">Dear [user_name],</div>
+						<p>
+						<div class="text-normal oscuro" style="font-size: 16px; color:#555;"> We have sent your E-dealcountry.com gift card to [cliente_email]. Please see below for a preview. </div>
+						</p>
+						<div>[imagen_separacion_inicio]</div>
+					</td>
+				</tr> -->
+				<tr class="preview_egiftcard">
+					<td>
+						<div class="text-normal claro"style="font-size: 16px; color:#777;">Dear [cliente_nombre],</div>
+						<p>
+						<div class="text-normal claro" style="font-size: 16px; color:#777;"> <i>[cliente_descripcion] </i></div>
+						</p>
+						<div class="text-normal claro"> <img src="[cliente_tarjeta]" alt=""> </div>
+						<div>[imagen_separacion_final]</div>
+					</td>
+				</tr>
+				<tr>
+					<td>Ready to use your E-dealcountry.com gift card?</td>
+				</tr>
+				<tr style="font-size: 16px; color:#555;">
+					<td>
+						<div class="listado" style="padding-left:20px">
+							<div>1. Go to <a href="http://dealcountry.com">www.dealcountry.com</a></div>
+							<div>2. Log into your account or Sign up for a new account</div>
+							<div>3. On "My Account" area , click on "Register a Gift Card"</div>
+							<div>4. Add your unique Gift Card number: [giftcard_number]</div>
+							<div>5. FInd the deal you want, click "Buy With Gift Card" and follow the instructions</div>
+						</div>
+						<p>Enjoy your shopping experience!</p>
+						<p>Thank you for using www.dealcountry.com</p>
+						<br>
+						<div style="font-size: 18px; color:#444;"><b>Customer Support</b></div>
+						<div>support@dealcountry.com</div>
+						<div>[LOGO]</div>
+					</td>
+				</tr>
+
+				<!-- <tr>
+					<td class="texto-normal oscuro centrado" style="font-size: 16px; color:#777;text-align:center"> 
+						<div>Thank you for using <a href="www.dealcountry.com">www.dealcountry.com</a> Gift Card system.</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table>
+							<tr  style="font-size: 18px; color:#777;text-align:center">
+								<td>[imagen_carita]</td>
+								<td>
+									<a href="www.dealcountry.com">NOW CLICK HERE</a> to go to your account and send another Gift Card or find some great deals for 
+									yourself!
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr> -->
+			</table>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ sites/all/modules/custom/giftcard/egiftcard-cliente.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<table>
+		<tr class="cabecera">
+			<td>[LOGO]</td>
+		</tr>
+		<tr>
+			<table class="cuerpo">
+				<!-- <tr>
+					<td class="text-grande">[cliente_nombre] has recieved your $50.00 E-dealcountry.com gift card.</td>
+				</tr> -->
+				<tr>
+					<td>
+						<div class="text-normal oscuro" style="font-size: 16px; color:#555;">Dear [user_name],</div>
+						<p>
+						<div class="text-normal oscuro" style="font-size: 16px; color:#555;"> We have sent your E-dealcountry.com gift card to [cliente_email]. Please see below for a preview. </div>
+						</p>
+						<div>[imagen_separacion_inicio]</div>
+					</td>
+				</tr>
+				<tr class="preview_egiftcard">
+					<td>
+						<div class="text-normal claro"style="font-size: 16px; color:#777;">Dear [cliente_nombre],</div>
+						<p>
+						<div class="text-normal claro" style="font-size: 16px; color:#777;"> <i>[cliente_descripcion] </i></div>
+						</p>
+						<div class="text-normal claro"> <img src="[cliente_tarjeta]" alt=""> </div>
+						<div>[imagen_separacion_final]</div>
+					</td>
+				</tr>
+				<tr>
+					<td class="texto-normal oscuro centrado" style="font-size: 16px; color:#777;text-align:center"> 
+						<div>Thank you for using <a href="www.dealcountry.com">www.dealcountry.com</a> Gift Card system.</div>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<table>
+							<tr  style="font-size: 18px; color:#777;text-align:center">
+								<td>[imagen_carita]</td>
+								<td>
+									<a href="www.dealcountry.com">NOW CLICK HERE</a> to go to your account and send another Gift Card or find some great deals for 
+									yourself!
+								</td>
+							</tr>
+						</table>
+					</td>
+				</tr>
+			</table>
+		</tr>
+	</table>
+</body>
+</html>
